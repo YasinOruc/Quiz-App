cbc6056a37f930ac9c0590198a4a4c852f092641
YasinOruc <orucyasin14@gmail.com>
2024-09-06 11:26:07 +0200

    commit

---
+++ script.js
@@ -0,0 +1,71 @@
+const questions = [
+    {
+        question: "What is the largest animal in the world?",
+        answers: [
+            { text: "Shark", correct: false },
+            { text: "Blue Whale", correct: true },
+            { text: "Elephant", correct: false },
+            { text: "Giraffe", correct: false },
+        ]
+    },
+    {
+        question: "Which planet is known as the Red Planet?",
+        answers: [
+            { text: "Jupiter", correct: false },
+            { text: "Mars", correct: true },
+            { text: "Venus", correct: false },
+            { text: "Mercury", correct: false },
+        ]
+    },
+    {
+        question: "What is the chemical symbol for gold?",
+        answers: [
+            { text: "Au", correct: true },
+            { text: "Ag", correct: false },
+            { text: "Pb", correct: false },
+            { text: "Fe", correct: false },
+        ]
+    },
+    {
+        question: "What is the capital city of Japan?",
+        answers: [
+            { text: "Seoul", correct: false },
+            { text: "Beijing", correct: false },
+            { text: "Tokyo", correct: true },
+            { text: "Bangkok", correct: false },
+        ]
+    },
+    {
+        question: "Which element has the highest electrical conductivity?",
+        answers: [
+            { text: "Copper", correct: false },
+            { text: "Silver", correct: true },
+            { text: "Gold", correct: false },
+            { text: "Iron", correct: false },
+        ]
+    },
+    {
+        question: "Who wrote 'Hamlet'?",
+        answers: [
+            { text: "William Shakespeare", correct: true },
+            { text: "Charles Dickens", correct: false },
+            { text: "Jane Austen", correct: false },
+            { text: "Ernest Hemingway", correct: false },
+        ]
+    }
+];
+
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next"
+    showQuestion();
+}
